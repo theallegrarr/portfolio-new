374b50284ce91640296ed836a6753682b66c0bbf
Nurudeen Amedu <nurudeen.amedu@gmail.com>
2021-08-31 09:35:24 +0100

    added badges

--- src/new/Home.jsx
+++ src/new/Home.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
+import {list} from './utils/badges'
 
 import "./style.css"
 
+/**
+ * 
+ * @todo
+ * 1, Latest Videos (YT API)
+ * 2, Latest Articles
+ * 3, My Activity (GitHub)
+ * 4, Tech Stack Icons 
+ */
+
 export default function HomeX() {
+
+
   return (<div>
     <div className="cta-text">
         <h3>
-            I’m Nurudeen, Frontend and Mobile Developer.
+            I’m Nurudeen, Full Stack Developer.
         </h3>
         <p className="more-info">
             I specialize in rapidly prototyping software companies and web applications. 
@@ -15,6 +27,14 @@ export default function HomeX() {
             commit code to <a className="git" href="https://github.com/theallegrarr"  target="_blank" rel="noopener noreferrer">Github</a>, 
             and tell stories on <a className="dev" href="https://dev.to/theallegrarr"  target="_blank" rel="noopener noreferrer">DEV</a>
         </p>
+    </div>
+    <div className="badges">
+        {list.map((item, i) => (
+            <div key={i} className="badge" id="java-badge">
+                <img className="icons" id={`${item.name}-icon`} src={item.icon} alt={item.name} />
+                <h4>{item.name}</h4>
+            </div>
+        ))}
     </div>
   </div>)
 }
---
+++ src/new/utils/badges.js
@@ -0,0 +1,85 @@
+import bootstrap from '../assets/tools/bootstrap.svg'
+import js from '../assets/tools/javascript.svg'
+import mongodb from '../assets/tools/mongodb.svg'
+import nodejs from '../assets/tools/nodejs.svg'
+import react from '../assets/tools/react-native.svg'
+import sass from '../assets/tools/sass.png'
+import swift from '../assets/tools/swift.svg'
+import webpack from '../assets/tools/webpack.svg'
+import typescript from '../assets/tools/typescript.svg'
+import redux from '../assets/tools/redux.svg'
+import gcp from '../assets/tools/google-cloud.svg'
+import pg from '../assets/tools/postgresql.svg'
+import c from '../assets/tools/c++.svg'
+import fb from '../assets/tools/firebase.svg'
+import git from '../assets/tools/git.svg'
+import heroku from '../assets/tools/heroku.svg'
+
+export const list = [
+    {
+        name: 'React/React Native',
+        icon: react,
+        icon_focus: null
+    },{
+        name: 'JavaScript',
+        icon: js,
+        icon_focus: null
+    },{
+        name: 'TypeScript',
+        icon: typescript,
+        icon_focus: null
+    },{
+        name: 'Node.js',
+        icon: nodejs,
+        icon_focus: null
+    },{
+        name: 'Swift/SwiftUI',
+        icon: swift,
+        icon_focus: null
+    },{
+        name: 'Git',
+        icon: git,
+        icon_focus: null
+    },{
+        name: 'MongoDB',
+        icon: mongodb,
+        icon_focus: null
+    },{
+        name: 'SASS',
+        icon: sass,
+        icon_focus: null
+    },{
+        name: 'webpack',
+        icon: webpack,
+        icon_focus: null
+    },{
+        name: 'Redux',
+        icon: redux,
+        icon_focus: null
+    },{
+        name: 'PostgreSQL',
+        icon: pg,
+        icon_focus: null
+    },{
+        name: 'C++',
+        icon: c,
+        icon_focus: null
+    },{
+        name: 'Firebase',
+        icon: fb,
+        icon_focus: null
+    },{
+        name: 'Bootstrap',
+        icon: bootstrap,
+        icon_focus: null
+    },{
+        name: 'Google Cloud',
+        icon: gcp,
+        icon_focus: null
+    },{
+        name: 'Heroku',
+        icon: heroku,
+        icon_focus: null
+    }
+]
+//git firebase postgres heroku c++
