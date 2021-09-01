f9281687e6a7c4c22efb11aa1aaf6e6f030b54ba
Nurudeen Amedu <nurudeen.amedu@gmail.com>
2021-09-01 12:55:29 +0100

    added mobile nav

--- src/new/Header.jsx
+++ src/new/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 import heart from './assets/Heart.svg'
 
 export default function Header(){
+    const [menuOpen, setMenuOpen] = useState(false)
+    const animate = () => {
+        setMenuOpen(!menuOpen)
+    }
 
     return(<div className="nav">
         <Link to="/"><img src={heart} alt="heart" />Nurudeen Amedu</Link>
@@ -11,6 +15,29 @@ export default function Header(){
             <Link to="/articles">Articles</Link>
             <Link to="/videos">Videos</Link>
             <Link to="/courses">Courses</Link>
+            <a href="mailto:nurudeen.amedu@gmail.com" className="nav-contact">
+                Let's Chat!
+            </a>
         </div>
+
+        <div className={`menu-btn ${menuOpen ? 'open' : ''}`} onClick={(e) => {
+            e.preventDefault()
+            animate()
+        }}>
+            <div className="menu-btn__burger"></div>
+        </div>
+
+        
+        <div className={`nav-wrapper ${menuOpen ? 'open' : ''}`}>
+            <ul className={`menu ${menuOpen ? 'open' : ''}`}>
+                <Link to="/articles">Articles</Link>
+                <Link to="/videos">Videos</Link>
+                <Link to="/courses">Courses</Link>
+                <a href="mailto:nurudeen.amedu@gmail.com" className="nav-contact">
+                    Let's Chat!
+                </a>
+            </ul>
+        </div>
+
     </div>)
 }
