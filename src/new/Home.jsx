6bb38da14027dacc0adc33df864f8913e8595070
Nurudeen Amedu <nurudeen.amedu@gmail.com>
2021-09-01 13:30:13 +0100

    home updates

--- src/new/Home.jsx
+++ src/new/Home.jsx
@@ -35,10 +35,10 @@ export default function HomeX() {
             I’m Nurudeen, Full Stack Developer.
         </h3>
         <p className="more-info">
-            I specialize in rapidly prototyping software companies and web applications. 
-            I talk about my journey on <a href="https://www.youtube.com/channel/UCvF499ChQBnWb5ex162EiMg"  target="_blank" rel="noopener noreferrer">Youtube</a>, 
-            commit code to <a className="git" href="https://github.com/theallegrarr"  target="_blank" rel="noopener noreferrer">Github</a>, 
-            and tell stories on <a className="dev" href="https://dev.to/theallegrarr"  target="_blank" rel="noopener noreferrer">DEV</a>
+            I specialize in rapidly devloping software applications for companies and individuals. 
+            Visualize my process on <a href="https://www.youtube.com/channel/UCvF499ChQBnWb5ex162EiMg"  target="_blank" rel="noopener noreferrer">Youtube</a>, 
+            check my repos <a className="git" href="https://github.com/theallegrarr"  target="_blank" rel="noopener noreferrer">Github</a>, 
+            and read my stories on <a className="dev" href="https://dev.to/theallegrarr"  target="_blank" rel="noopener noreferrer">DEV</a>
         </p>
     </div>
     <div className="badges">
